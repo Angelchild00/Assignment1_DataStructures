8b7a595aca564dae122cbb9442b682b02da242cf
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-02-10 15:28:35 -0500

    Added in Main.c
    Created program to run functions

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
-#include <stdio.h>
 #include "queue.h"
+#include <time.h>
 
+int main(int argc, char* argv[]) {
+	//check arguments
+	if (argc != 2) {
+		printf("%s <number of players>\n", argv[0]);
+		return 1;
+	}
+	int numPlayers = atoi(argv[1]);
+	if (numPlayers <= 0) {
+		printf("invalid number of players\n");
+		return 1;
+	}
+	srand(time(NULL));
 
-int main() {
+	Queue q;
+	initializeQueue(&q);
+	
+	printf("Enqueuing %d players\n", numPlayers);
+	fillQueue(&q, numPlayers);
 
+	printf("Dequeuing players: \n");
+	while (!isEmpty(&q)) {
+		Player player = dequeue(&q);
+		printf("Player Name: %s , Level: %d , Faction: %s\n", player.playerName, player.level, getFactionName(player.faction)); 
+	}
 
+	printf("Queue is now empty, ending program\n");
 	return 0; 
 }
