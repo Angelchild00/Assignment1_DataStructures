c77db55fac09ed33b9a7df415ea0f75ef3c8ca32
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-02-10 11:03:52 -0500

    Created queue.h
    Created prototypes, structs and enum

--- queue.h
+++ queue.h
@@ -1,1 +1,46 @@
 #pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PLAYER_NAME_LENGTH 11
+
+// Enum for Factions
+typedef enum {
+    RED,   // 0
+    BLUE,  // 1
+    GREEN  // 2
+} Faction;
+
+// Player struct
+typedef struct Player {
+    char playerName[PLAYER_NAME_LENGTH];
+    int level;
+    Faction faction;
+} Player;
+
+// Queue Node struct
+typedef struct QueueNode {
+    Player player;
+    struct QueueNode* next;
+} QueueNode;
+
+// Queue struct
+typedef struct Queue {
+    QueueNode* head;
+    QueueNode* tail;
+} Queue;
+
+// Function Prototypes
+void initializeQueue(Queue* q);
+int isEmpty(Queue* q);
+void enqueue(Queue* q, Player player);
+Player dequeue(Queue* q);  // Fixed function signature
+Player createPlayer();
+void fillQueue(Queue* q, int numPlayers);
+const char* getFactionName(Faction faction);
+
+#endif // QUEUE_H
