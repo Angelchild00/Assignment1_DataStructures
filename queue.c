159e0b03ec662207c9115d3455cf381945dd5f42
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-02-10 13:50:28 -0500

    added functions
    Added in functions for initialization, empty, enqueue, dequeue

--- queue.c
+++ queue.c
@@ -1,16 +1,39 @@
 #include "queue.h"
 
-void initializeQueue(Queue* q) {
-
+void initializeQueue(Queue* q) { //initialize queue
+	q->head = NULL;
+	q->tail = NULL;
 }
-int isEmpty(Queue* q) {
-
+int isEmpty(Queue* q) { // check if queue is empty
+	if (q->head == NULL) {
+		return 1;
+	}
+	return 0;
 }
 void enqueue(Queue* q, Player player) {
-
+	QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
+	if (!newNode) {
+		printf("Malloc Failed\n");
+		exit(EXIT_FAILURE);
+	}
+	newNode->player = player;
+	newNode->next = NULL;
 }
 Player dequeue(Queue* q) {
+	if (isEmpty(q)) {
+		printf("Queue is empty, dequeue failed\n");
+		exit(EXIT_FAILURE);
+	}
 
+	QueueNode* temp = q->head;
+	Player player = temp->player;
+	q->head = q->head->next;
+
+	if (q->head == NULL) {
+		q->tail = NULL;
+	}
+	free(temp);
+	return player; 
 }
 Player createPlayer() {
 
